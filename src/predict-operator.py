084416f461df58dc31d69c2ae1055f33e83ef6e0
HstSethi <hstsethi@outlook.com>
2024-10-11 00:20:50 -0400

    Implement Argparsing, improve doc, return a scalar numpy array instead of dataframe

--- src/predict-operator.py
+++ src/predict-operator.py
@@ -1,10 +1,11 @@
+import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score 
 import joblib
-import os
+import argparse
 
 def check_series_in_database(series_to_predict: str, database_filename):
     """
@@ -23,6 +24,7 @@ def check_series_in_database(series_to_predict: str, database_filename):
         return result_operator
 
 def train_save_model(model_name, database_filename):
+    """ Train models for  specified prefix using KNN classifier and save them"""
     data = pd.read_csv(database_filename)
     data.dropna(inplace=True)
     data.drop_duplicates(inplace=True)
@@ -34,33 +36,36 @@ def train_save_model(model_name, database_filename):
     train_acc = accuracy_score(train_pred, y_train)
     test_acc = accuracy_score(test_pred, y_test)
     print("Training accuracy:", train_acc, "\n", "Testing accuracy:,", test_acc)
-    print(f"Saved {model_name} to disk")
     joblib.dump(knn, model_name)
+    print(f"Saved {model_name} to disk")
 
 def predict_using_saved_model(model_to_load, series_to_predict):
+    """Make predictions about operator using pretrained model"""
     model = joblib.load(model_to_load)
     series_to_predict_array = np.array(int(series_to_predict)).reshape(-1,1) # predict() method takes an array instead of a scalar
     predicted_operator = model.predict(series_to_predict_array)
     return predicted_operator
 
 def main():
-    # Implement Arg Parsing
-    # database_filename = "../data/9xxx-in-mob-prefix.csv"
-    database_filename = "../data/8xxx-in-mob-prefix.csv"
-    # model_to_load = "../models/9xxx-knn.bin"
-    model_to_load = "../models/8xxx-knn.bin"
-    series_to_predict = "8000"
+
+    parser = argparse.ArgumentParser(description = "Script to predict/lookup operator name based on series. Part of in-mob-prefix project by HstSethi <hstsethi@outlook.com>")
+    parser.add_argument("database", help=".csv database to use")
+    parser.add_argument("series", help="Series to predict")
+    parser.add_argument("model", help="Model to load")
+    args = parser.parse_args()
+    database_filename = args.database
+    model_to_load = args.model
+    series_to_predict = args.series
+
     operator = check_series_in_database(series_to_predict, database_filename)
     if operator is not None:
-        print("Operator Found in Database")
-        print(operator) # Print on a seperate line so it's easier to grep
-        return 0
-    if os.path.exists(model_to_load):
+        print("Operator Found in Database\n", operator) # Print on a seperate line so it's easier to grep
+    elif os.path.exists(model_to_load):
         predicted_operator = predict_using_saved_model(model_to_load, series_to_predict)
         print("Predicted Operator:", predicted_operator)
     else:
-        raise(FileNotFoundError(model_to_load, "Does not exist. Training"))
+        print(f"Error: {model_to_load} Does not exist. Starting model training")
         train_save_model(model_to_load, database_filename)
-        predict_using_saved_model(model_name, series_to_predict)
+        predict_using_saved_model(model_to_load, series_to_predict)
 
 main() 
